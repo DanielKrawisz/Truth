99c3281100e0154cb303e495a0dc41d62a6f9034
DanielKrawisz <daniel.krawisz@thingobjectentity.net>
2019-05-17 22:10:50 -0500

    Add comments to intuitionistic logic

--- include/truth/logic/intuitionistic.hpp
+++ include/truth/logic/intuitionistic.hpp
@@ -5,20 +5,40 @@
 #ifndef TRUTH_LOGIC_INTUINISTIC
 #define TRUTH_LOGIC_INTUINISTIC
 
-#include <truth/contradiction.hpp>
-
 namespace truth {
     
-    namespace logic {
+    // Intuitionistic logic is the framework that connects programming to proofs. 
+    // In intuitionistic logic, a proposition P is always a claim not only that 
+    // P is true, but that a proof exists for P. This makes intuitionistic logic
+    // more restrictive than the Boolean logic that programmers are more 
+    // familiar with. 
     
+    namespace logic {
+        
+        // Intuitionistic logic uses 5 words, although Not is defined in terms of
+        // of other words so there are really only four independent words. 
+    
+        // And means that all parameter types have proofs. 
         template <typename ...> struct And;
     
+        // Or means that at least one parameter type has a proof. 
         template <typename ...> struct Or;
         
+        // Implies is a function which transforms one kind of proof into another. 
         template <typename, typename ...> struct Implies;
+        
+        // Contradiction is an unconstructible type. 
+        struct Contradiction;
+        
+        // Not<P> means that a proof of P would imply that a contradiction can be constructed. 
+        template <typename X> using Not = Implies<X, Contradiction>;
     
+        // Because it is possible for neither a proposition nor its negation to have a proof, 
+        // in is not the case that P || !P in intuitionistic logic nor is it the case that 
+        // !!P => P. 
+        
         namespace intuitionistic {
-                
+            
             template <typename x, typename y>
             extern Implies<x, y, x> axiom_then_1;
                 
